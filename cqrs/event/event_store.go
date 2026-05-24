7ab6877cbeb2d2038df091fc7a21acf657b23ea1
wangyaochong <1162025261@qq.com>
2026-05-24 19:58:18 +0800

    refactor: cqrs/event.EventStore[T] becomes type alias for domain/event.EventStore[T]

--- cqrs/event/event_store.go
+++ cqrs/event/event_store.go
@@ -1,12 +1,7 @@
 package event
 
 import (
-	"context"
-
-	"github.com/ddd-qce/core/domain/event"
+	domainevent "github.com/ddd-qce/core/domain/event"
 )
 
-type EventStore[T event.DomainEvent] interface {
-	Append(ctx context.Context, aggregateID string, expectedVersion int, events []T) error
-	Load(ctx context.Context, aggregateID string, afterVersion int) ([]T, error)
-}
+type EventStore[T domainevent.DomainEvent] = domainevent.EventStore[T]
